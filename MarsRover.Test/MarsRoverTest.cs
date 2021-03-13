a4fa2c0eaef3cd1aa15d1354d2770b80bb49f1da
DESKTOP-6L63B5I\oguzo <oguz.onal.50@gmail.com>
2021-03-13 23:27:23 +0300

    Test_Scenario_Max55_33E_MRRMMRMRRM

--- MarsRover.Test/MarsRoverTest.cs
+++ MarsRover.Test/MarsRoverTest.cs
@@ -18,7 +18,7 @@ namespace MarsRover.Test
                                .AddTransient<IRover, Rover>()
                                .BuildServiceProvider();
 
-            var maxLÝmits = new List<int>() { 5, 5 };
+            var maxLimits = new List<int>() { 5, 5 };
             Coordinate coordinate = new Coordinate
             {
                 Direction = Directions.N,
@@ -27,10 +27,32 @@ namespace MarsRover.Test
             };
             string moves = "LMLMLMLMM";
             IRover _rover = serviceProvider.GetService<IRover>();
-            var output = _rover.Move(maxLÝmits, moves, coordinate);
+            var output = _rover.Move(maxLimits, moves, coordinate);
 
             var actual = $"{output.X} {output.Y} {output.Direction}";
             var expected = "1 3 N";
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Test_Scenario_Max55_33E_MRRMMRMRRM()
+        {
+            ServiceProvider serviceProvider = new ServiceCollection()
+                               .AddTransient<IRover, Rover>()
+                               .BuildServiceProvider();
+
+            var maxLimits = new List<int>() { 5, 5 };
+            Coordinate coordinate = new Coordinate
+            {
+                Direction = Directions.E,
+                X = 3,
+                Y = 3
+            };
+            var moves = "MRRMMRMRRM";
+            IRover _rover = serviceProvider.GetService<IRover>();
+            var output = _rover.Move(maxLimits, moves, coordinate);
+
+            var actual = $"{output.X} {output.Y} {output.Direction}";
+            var expected = "2 3 S";
             Assert.AreEqual(expected, actual);
         }
     }
