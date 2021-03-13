e990884365170b5435568d3e5369397bd1122cb5
DESKTOP-6L63B5I\oguzo <oguz.onal.50@gmail.com>
2021-03-14 00:13:42 +0300

    Code Refactoring

--- MarsRover.ConsoleApp/Program.cs
+++ MarsRover.ConsoleApp/Program.cs
@@ -10,44 +10,57 @@ namespace MarsRover.ConsoleApp
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            ServiceProvider serviceProvider = new ServiceCollection()
-                   .AddTransient<IRover, Rover>()
-                   .BuildServiceProvider();
-
-            Console.WriteLine("X koordinatındaki Max Sınırı Giriniz..........:");
-            int maxX = Convert.ToInt32(Console.ReadLine().Trim());
-            Console.WriteLine("Y koordinatındaki Max Sınırı Giriniz..........:");
-            int maxY = Convert.ToInt32(Console.ReadLine().Trim());
-            Console.WriteLine("Başlangıç X koordinatını Giriniz........:");
-            int startX = Convert.ToInt32(Console.ReadLine().Trim());
-            Console.WriteLine("Başlangıç Y koordinatını Giriniz........:");
-            int startY = Convert.ToInt32(Console.ReadLine().Trim());
-            Console.WriteLine("Başlangıç Yönünü(N,S,W,E) Giriniz........:");
-            string startDirection = Console.ReadLine().Trim();
-            Console.WriteLine("Hareketleri(M,L,R) Giriniz........:");
-            string moves = Console.ReadLine().Trim().ToUpper();
-            List<int> maxLimits = new List<int>() {
-                maxX,maxY
-            };
-
             try
             {
+                ServiceProvider serviceProvider = new ServiceCollection()
+                   .AddTransient<IRover, Rover>()
+                   .BuildServiceProvider();
+                Input input = new Input();
+
+                SetInputsFromConsole(input);
+
+                List<int> maxLimits = new List<int>() {
+                    input.MaxX,input.MaxY
+                };
+
                 Coordinate coordinate = new Coordinate
                 {
-                    X = startX,
-                    Y = startY,
-                    Direction = (Directions)Enum.Parse(typeof(Directions), startDirection)
+                    X = input.StartX,
+                    Y = input.StartY,
+                    Direction = (Directions)Enum.Parse(typeof(Directions), input.StartDirection)
                 };
                 IRover _rover = serviceProvider.GetService<IRover>();
-                _rover.Move(maxLimits, moves, coordinate);
+                _rover.Move(maxLimits, input.Moves, coordinate);
                 Console.WriteLine($"{coordinate.X} {coordinate.Y} {coordinate.Direction}");
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        public static void SetInputsFromConsole(Input input)
+        {
+            Console.WriteLine("X koordinatındaki Max Sınırı Giriniz..........:");
+            input.MaxX = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Console.WriteLine("Y koordinatındaki Max Sınırı Giriniz..........:");
+            input.MaxY = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Console.WriteLine("Başlangıç X koordinatını Giriniz........:");
+            input.StartX = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Console.WriteLine("Başlangıç Y koordinatını Giriniz........:");
+            input.StartY = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Console.WriteLine("Başlangıç Yönünü(N,S,W,E) Giriniz........:");
+            input.StartDirection = Console.ReadLine().Trim();
+
+            Console.WriteLine("Hareketleri(M,L,R) Giriniz........:");
+            input.Moves = Console.ReadLine().Trim().ToUpper();
         }
     }
 }
---
+++ MarsRover.Domain/Model/Input.cs
@@ -0,0 +1,12 @@
+﻿namespace MarsRover.Domain.Model
+{
+    public class Input
+    {
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+        public int StartX { get; set; }
+        public int StartY { get; set; }
+        public string StartDirection { get; set; }
+        public string Moves { get; set; }
+    }
+}
