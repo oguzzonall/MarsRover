212a41e7b5ebe285e38ec76f035e176c284719af
DESKTOP-6L63B5I\oguzo <oguz.onal.50@gmail.com>
2021-03-13 21:42:19 +0300

    Add project files.

---
+++ MarsRover.Test/MarsRoverTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsRover.Test
+{
+    [TestClass]
+    public class MarsRoverTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
